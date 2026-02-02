37cf01ccbfd1f02ed3383edd45d5c25daad791e6
HaselmutGames <haselmut.games@gmail.com>
2026-02-02 09:05:47 +0100

    initial commit

---
+++ src/test/java/com/rtfm/hammer/RoleplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtfm.hammer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
