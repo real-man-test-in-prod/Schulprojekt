2953913dc6110c76afa4d65386e7e9f6e0d947a8
maher <maher15sus@gmail.com>
2026-02-13 12:52:08 +0100

    chore: save local controller changes before merging homepage branch

--- src/main/java/com/rtfm/hammer/controller/SchoolController.java
+++ src/main/java/com/rtfm/hammer/controller/SchoolController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SchoolController {
 
-    @GetMapping("/")
+    @GetMapping("/school")
     public String home() {
         return "school";
     }
