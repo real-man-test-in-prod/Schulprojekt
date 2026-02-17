14720c11c0b9f9c10083663fffdf025d2651b44a
maher <maher15sus@gmail.com>
2026-02-17 11:16:15 +0100

    #3 Refactor: Consolidate Room Templates and Variables passed to HTML added to roomController

--- src/main/java/com/rtfm/hammer/controller/RoomController.java
+++ src/main/java/com/rtfm/hammer/controller/RoomController.java
@@ -1,13 +1,28 @@
 package com.rtfm.hammer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class RoomController {
     @GetMapping("/rooms/{roomId}")
-        public String room1(@PathVariable String roomId) {
-            return "rooms/" + roomId;
+        public String room(@PathVariable String roomId, Model model) {
+        String roomText;
+        String roomDescription = "";
+
+        if (roomId.equals("room_6")) {
+            roomText = "Willkommen im Netzwerk-Raum Raum 6";
+            roomDescription="Hier gehen wir die Netzwerk themen durch";
+        } else {
+            roomText = "Unbekannter Raum ergebnis: "+ roomId;
+
+        }
+        model.addAttribute("roomText", roomText);
+        model.addAttribute("roomId", roomId);
+        model.addAttribute("roomDescription", roomDescription);
+
+        return "rooms/room";
         }
 }
