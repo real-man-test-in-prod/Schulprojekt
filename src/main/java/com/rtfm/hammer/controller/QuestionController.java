5d91d09d388feb90c0537c6cd769d4bad85d6c9e
Eckart <e.scheeren@animekultur.de>
2026-03-16 08:50:55 +0100

    Question can now be validated

--- src/main/java/com/rtfm/hammer/controller/QuestionController.java
+++ src/main/java/com/rtfm/hammer/controller/QuestionController.java
@@ -1,14 +1,14 @@
 package com.rtfm.hammer.controller;
 
 
-import com.rtfm.hammer.dto.QuestionRequest;
 import com.rtfm.hammer.model.Question;
 import com.rtfm.hammer.service.QuestionService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/question")
@@ -20,35 +20,11 @@ public class QuestionController {
         this.questionService = questionService;
     }
 
-    @GetMapping("/{questionID}")
+    @GetMapping("{questionID}")
     public Question getQuestion(@PathVariable String questionID) {
-        int code;
-
-        try {
-            code = Integer.parseInt(questionID);
-        } catch (NumberFormatException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid question code");
+        if (questionID.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "ID cannot be empty!");
         }
-
-        return questionService.getQuestionByCode(code);
-    }
-
-    @PostMapping("/{questionID}")
-    public int validateQuestion(@PathVariable String questionID, @RequestBody QuestionRequest body) {
-        int id = Integer.parseInt(questionID);
-        Question question = questionService.getQuestionByCode(id);
-        boolean isCorrect = false;
-
-        if (question.getQuestionType().equals("TF") || question.getQuestionType().equals("MC")) {
-            int test = questionService.validateMC(question, questionID, body.getAnswers());
-            System.out.println(test);
-        } else {
-            if (body.getAnswers().size() > 1) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Zu viele Antworten");
-            }
-            Map.Entry<String, String> field = body.getAnswers().entrySet().iterator().next();
-            isCorrect = questionService.validateGAP(field.getKey(), field.getValue());
-        }
-        return isCorrect ? question.getPoints() : 0;
+        return questionService.getQuestion(Integer.parseInt(questionID));
     }
 }
